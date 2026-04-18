bb69c1f28c219bc40b87f47273fc5953947d8df7
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-18 19:00:45 +0200

    fix: exception message

--- ex04/load_image.py
+++ ex04/load_image.py
@@ -7,7 +7,7 @@ def ft_load(path: str) -> numpy.ndarray:
     img = PIL.Image.open(path)
     fmt = img.format
     if fmt not in ["JPEG"]:  # jpg et jpeg renvoit JPEG
-        raise Exception(...)
+        raise Exception("not the right format (jpeg expected)"))
     img = img.convert("RGB")
     tab = numpy.array(img)
     print(f"The shape of image is: {tab.shape}")
