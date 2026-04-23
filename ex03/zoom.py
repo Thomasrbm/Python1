11843108623c3cf367e8e8f54a100739f48ac245
ThomasRbm <frozyxyt.76@gmail.com>
2026-04-23 13:11:33 +0200

    Update zoom.py

--- ex03/zoom.py
+++ ex03/zoom.py
@@ -3,30 +3,38 @@ import numpy
 import matplotlib.pyplot as plt
 
 
-# tab.shape[0]  #   → hauteur (axe Y)
-# tab.shape[1]  #  → largeur (axe X)
-# tab.shape[2]  # 3    → nombre de canaux (R, G, B)
-def zoom(tab: numpy.ndarray) -> numpy.ndarray:
-    y_center = tab.shape[0] // 2
-    x_center = tab.shape[1] // 2
-    sliced = tab[y_center-300:y_center+100, x_center-50:x_center+350, 1:2]
-    print(f"New shape after slicing: {sliced.shape}")
-    print(sliced)
-    return sliced
+def print_corners(tab):
+    first = tab[0, :3]
+    last = tab[-1, -3:]
+    fake = numpy.zeros_like(first[:1])
+    stacked = numpy.concatenate([first, fake, last])[numpy.newaxis, ...]
+    with numpy.printoptions(threshold=6, edgeitems=3):
+        print(stacked)
+
+
+def zoom(tab: numpy.ndarray):
+    print_corners(tab)
+    tab = tab[100:500, 400:800]
+    tab = tab[:, :, 1:2]
+    # si 3 cannaux cmap est ignored
+    print(f"New shape after slicing: {tab.shape}")
+    with numpy.printoptions(edgeitems=3, threshold=10):
+        print(tab[:1])
+    return tab
+
+
+def main():
+    try:
+        img = load_image.ft_load("animal.jpeg")
+        sliced = zoom(img)
+        plt.imshow(sliced, cmap="gray")
+        # load l objet AxesImage, dans la fenetre Figure
+        plt.show()
+    except KeyboardInterrupt:
+        print("Ctrl + c interrupted")
+    except Exception as e:
+        print(f"error: {e}")
 
 
 if __name__ == "__main__":
-    try:
-        tab = load_image.ft_load("animal.jpeg")
-        print(tab)
-        sliced = zoom(tab)
-        plt.imshow(numpy.squeeze(sliced), cmap="gray")
-        plt.axis('on')  # met en pixel
-        plt.show()  # ouvre la fenetre
-    except Exception as e:
-        print(f"Error: {e}")
-
-# numpy.squeeze → enlève les dimensions de taille 1
-#                 (400,400,1) devient (400,400)
-
-# car mode gray scal attend 2d
+    main()
